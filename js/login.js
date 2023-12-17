d4d4e9ea5278ccb6e2a8e91f21de21a1ff942c57
FrancoMosca <franco.mos@gmail.com>
2023-12-16 23:50:56 -0300

    ultimo commit

--- js/login.js
+++ js/login.js
@@ -1,81 +1,79 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
-import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut,
-  } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
+import { existsSync, readFileSync } from "fs";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBojzze7jEKUU9eItTb415B5_VY0B0Qxv4",
-    authDomain: "login-proyecto-coderhouse.firebaseapp.com",
-    projectId: "login-proyecto-coderhouse",
-    storageBucket: "login-proyecto-coderhouse.appspot.com",
-    messagingSenderId: "400474600977",
-    appId: "1:400474600977:web:94e7c0ae14fe8e47b7e3b0",
-  };
+const BD_PATH = "../users.json";
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+const userEmail = document.querySelector("#userEmail");
+const userPassword = document.querySelector("#userPassword");
+const authForm = document.querySelector("#authForm");
+const secretContent = document.querySelector("#secretContent");
+const signUpButton = document.querySelector("#signUpButton");
+const signInButton = document.querySelector("#signInButton");
+const signOutButton = document.querySelector("#signOutButton");
 
-  const userEmail = document.querySelector("#userEmail");
-  const userPassword = document.querySelector("#userPassword");
-  const authForm = document.querySelector("#authForm");
-  const secretContent = document.querySelector("#secretContent");
-  const signUpButton = document.querySelector("#signUpButton");
-  const signInButton = document.querySelector("#signInButton");
-  const signOutButton = document.querySelector("#signOutButton");
+const userSignIn = async () => {
+  const signInEmail = userEmail.value;
+  const signInPassword = userPassword.value;
 
-  const userSignUp = async () => {
-    const signUpEmail = userEmail.value;
-    const signUpPassword = userPassword.value;
-    createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        alert("Your account has been created!");
-        checkAuthState();
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode + errorMessage);
-      });
-  };
+  const response = await fetch("../users.json");
+  const users = await response.json();
+  console.log(users)
 
-  const userSignIn = async () => {
-    const signInEmail = userEmail.value;
-    const signInPassword = userPassword.value;
-    signInWithEmailAndPassword(auth, signInEmail, signInPassword)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        alert("You have signed in successfully!");
-        checkAuthState();
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode + errorMessage);
-      });
-  };
+  if (
+    !users.find(
+      (user) => user.user.username === signInEmail && user.user.password === signInPassword
+    )
+  ) {
+    Swal.fire("User does not exist or credentials are incorrect", "error");
+    return;
+  }
 
-  const checkAuthState = async () => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        window.location.href = "./home.html";
-      } else {
+  onLoginSuccess();
+};
 
-      }
-    });
-  };
+const userSignUp = () => {
+  const users = [];
 
-  const userSignOut = async () => {
-    await signOut(auth);
-    checkAuthState();
-  };
+  console.log("existe el archivo? "+existsSync(BD_PATH))
 
-  
+  try {
+    const data = readFileSync(BD_PATH, "utf-8");
+    users = JSON.parse(data);
+  } catch (e) {
+    console.log("File does not exists");
+    return;
+  }
 
-  signUpButton.addEventListener("click", userSignUp);
-  signInButton.addEventListener("click", userSignIn);
+  const signInEmail = userEmail.value;
+  const signInPassword = userPassword.value;
+
+  if (
+    users.find(
+      (user) => user.user.username === signInEmail && user.user.password === signInPassword
+    )
+  ) {
+    Swal.fire("User already exists","error");
+    return;
+  }
+
+  users.push({ user: { username: signInEmail, password: signInPassword } });
+  onLoginSuccess();
+};
+
+
+const onLoginSuccess = (msg) => {
+  Swal.fire("You have signed in successfully!", "success");
+  window.location.href = "./home.html";
+};
+
+
+signUpButton.addEventListener("click", userSignUp);
+signInButton.addEventListener("click", userSignIn);
+
+// const users = [];
+
+// async function getUsers() {
+//   const response = await fetch("../users.json");
+//   const users = await response.json();
+// }
+
+// getUsers();
