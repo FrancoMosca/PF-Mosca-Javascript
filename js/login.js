66992921bf354e014e942f805180925188d61f96
FrancoMosca <franco.mos@gmail.com>
2024-01-03 15:41:08 -0300

    final commit

--- js/login.js
+++ js/login.js
@@ -1,10 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
 } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 const firebaseConfig = {
@@ -13,10 +13,8 @@ const firebaseConfig = {
   projectId: "login-proyecto-coderhouse",
   storageBucket: "login-proyecto-coderhouse.appspot.com",
   messagingSenderId: "400474600977",
-  appId: "1:400474600977:web:94e7c0ae14fe8e47b7e3b0"
+  appId: "1:400474600977:web:94e7c0ae14fe8e47b7e3b0",
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -27,53 +25,52 @@ const authForm = document.querySelector("#authForm");
 const signUpButton = document.querySelector("#signUpButton");
 const signInButton = document.querySelector("#signInButton");
 
-const userSignUp = async() => {
-    const signUpEmail = userEmail.value;
-    const signUpPassword = userPassword.value;
-    createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
+const userSignUp = async () => {
+  const signUpEmail = userEmail.value;
+  const signUpPassword = userPassword.value;
+  createUserWithEmailAndPassword(auth, signUpEmail, signUpPassword)
     .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-        alert("Your account has been created!");
+      const user = userCredential.user;
+      console.log(user);
+      Swal.fire("Creada", "Tu cuenta ha sido creada", "success");
     })
     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode + errorMessage)
-    })
-}
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode + errorMessage);
+      Swal.fire("Error", errorMessage, "Error");
+    });
 
-const userSignIn = async() => {
-    const signInEmail = userEmail.value;
-    const signInPassword = userPassword.value;
-    signInWithEmailAndPassword(auth, signInEmail, signInPassword)
+};
+
+const userSignIn = async () => {
+  const signInEmail = userEmail.value;
+  const signInPassword = userPassword.value;
+  signInWithEmailAndPassword(auth, signInEmail, signInPassword)
     .then((userCredential) => {
-        const user = userCredential.user;
-        alert("You have signed in successfully!");
+      const user = userCredential.user;
+      Swal.fire("Exito", "Has iniciado sesion", "success");
     })
     .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode + errorMessage)
-    })
-}
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode + errorMessage);
+      Swal.fire("Error", errorMessage);
+    });
+};
 
-const checkAuthState = async() => {
-    onAuthStateChanged(auth, user => {
-        if(user) {
-          window.location.href = "./home.html";
-        }
-        else {
-          console.error("error")
-        }
-    })
-}
-
-const userSignOut = async() => {
-    await signOut(auth);
-}
+const checkAuthState = async () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      window.location.href = "./home.html";
+    } else {
+      Swal.fire("Error", "No has podido iniciar sesion", "Error");
+    }
+  });
+};
 
 checkAuthState();
 
-signUpButton.addEventListener('click', userSignUp);
-signInButton.addEventListener('click', userSignIn);
+
+signUpButton.addEventListener("click", userSignUp);
+signInButton.addEventListener("click", userSignIn);
